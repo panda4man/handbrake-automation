5ae9373bd6c79430d3edfd0f953e07c5873f9ddb
Andrew Clinton <ddrew.clinton@gmail.com>
2025-01-02 09:58:49 -0500

    fix colors

--- resources/views/livewire/failed-compressions.blade.php
+++ resources/views/livewire/failed-compressions.blade.php
@@ -14,7 +14,13 @@
     @endif
 
     @if ($failed_compressions->isEmpty())
-        <p class="text-gray-600">No failed compressions at the moment.</p>
+        <div class="flex items-center space-x-2 p-4 bg-gray-100 rounded shadow">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z" />
+            </svg>
+
+            <p class="text-red-800 font-medium">No pending compressions at the moment.</p>
+        </div>
     @else
         <ul class="space-y-4">
             @foreach ($failed_compressions as $compression)
